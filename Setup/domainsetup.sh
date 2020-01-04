29fae834557a5366887132b753b8c252a9b99e73
webhostsg <59085206+webhostsg@users.noreply.github.com>
2020-01-04 21:18:20 +0800

    Update domainsetup.sh

--- Setup/domainsetup.sh
+++ Setup/domainsetup.sh
@@ -323,7 +323,10 @@ main_upgrade(){
            
     else
         echoG 'Your system is up to date'
-        echoY "\n***Setup completed. You can close this and proceed to follow the instructions in the email.***"
+        echo -e "********************************************************************************"
+        echoY -e "                     Congratulations! Setup has completed.                      "
+        echoY -e "    You can close this and proceed to follow the instructions in the email.     "
+        echo -e "********************************************************************************"
     fi        
 }
 
